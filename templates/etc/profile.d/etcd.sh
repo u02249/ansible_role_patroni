c48dfc5d255a059ae9574149da45f2f225e5b31a
Sverchkov Evgeniy <you@example.com>
2019-11-12 17:40:30 +0600

    add etcd config

--- templates/etc/profile.d/etcd.sh
+++
@@ -1,2 +0,0 @@
-export ETCD_INITIAL_CLUSTER="ubuntu-bionic-1=http://10.128.0.2:2380,ubuntu-bionic-2=http://10.128.0.3:2380,ubuntu-bionic-3=http://10.128.0.4:2380"
-export ETCD_INITIAL_CLUSTER_STATE=new
